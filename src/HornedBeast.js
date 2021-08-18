3f5c9592f6d9221db4c03d57544d1a4ad6854070
Justin Hamerly <suvimountain@icloud.com>
2021-08-18 12:14:11 -0700

    css styling for animal articles

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import Accordion from 'react-bootstrap/Accordion';
+import heart from './assets/heart.png';
 
 class HornedBeast extends Component {
 
@@ -20,23 +21,20 @@ class HornedBeast extends Component {
   render(){
     return(
       <article class='beastarticle'>
-        <div class='articleimg'>
-          <img src={this.props.image_url} alt={this.props.keyword}></img>
-          <Accordion>
-            <Accordion.Item eventKey="0">
-              <Accordion.Header>{this.props.title}</Accordion.Header>
-              <Accordion.Body>
-                <p>{this.props.description}</p>
-                <p>Horns: {this.props.horns}</p>
-              </Accordion.Body>
-            </Accordion.Item>
-          </Accordion>
-        </div>
+        <Accordion>
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>{this.props.title}</Accordion.Header>
+            <Accordion.Body>
+              <p>{this.props.description}</p>
+              <p>Horns: {this.props.horns}</p>
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+        <img src={this.props.image_url} alt={this.props.keyword}></img>
         <div class='votesection'>
+          <p><img class="heart" src={heart} alt="heart" /> {this.state.votes}</p>
           <button class="votebutton" onClick={this.handleClick}>Vote For Me!</button>
-          <p><img class="heart" src="https://i.ibb.co/St4DL3r/heart.png" alt="heart" /> {this.state.votes}</p>
         </div>
-
       </article>
     );
   }
